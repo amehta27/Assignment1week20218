47a2a56f9c682b6fae74c2716372ca1e742fbda5
amehta27 <apmk_27@yahoo.com>
2019-02-19 21:47:42 -0600

    revised

--- GroceryApp.py
+++ GroceryApp.py
@@ -1,52 +1,128 @@
-AllStoreList = []
+user_input = ""
+storeslist = []
+
+class Store:
+  def __init__(self,name,address):
+     self.name = name
+     self.address = address
+     self.grocery_items = []
+
+class GroceryItem:
+    def __init__(self,name,quantity,price):
+        self.name = name
+        self.quantity = quantity
+        self.price = price
 
 def show_menu():
-    print("Press 1 to add a store:")
-    print("Press 2 to add more item to the store :")
-    print("Press 3 to view all the stores:")
-    print("Press q to quit:")
-
+    print("Press 1 to add stores:")
+    print("Press 2 to add grocery items:")
+    print("Press 3 to dispaly all the grocery items from the all the stores.")
+    print("Press q to exit")
+    # print("Please input only numbers..")
 
 def add_store():
-    storename = input("Enter the store name:")
-    shopping_list1 = Shopping_List(storename)
-    AllStoreList.append(shopping_list1)
+    storename = input("Enter the name of the store:")
+    storeaddress = input("Enter the address of the store")
+    store = Store(storename,storeaddress )
+    #print(f"{store.name} {store.address}")
+    storeslist.append(store)
+    #print(storeslist)
 
-class Shopping_List:
-    def __init__(self,name_store):
-        self.name_store = name_store
-        self.grocery_items = []
-    def add_item(self):
-        for index in range(0,len(AllStoreList)):
-            print (f"{index + 1}-{AllStoreList[index].name_store}")
-        additem = input("add your item:")
-        addquantity = input("add your quantity:")
-        addprice = input("add price")
-        grocery_item1 =  Grocery_Item(additem,addquantity,addprice)
-        self.grocery_items.append(grocery_item1)
+def display_all_items():
+    for index in range(0,len(storeslist)):
+        storeelement = storeslist[index]
+        print(f"{index+1} - {storeelement.name} - {storeelement.address}")
+        totalprice = 0
+        for grocery_item in storeelement.grocery_items:
+            print(f"  {grocery_item.name} -  {grocery_item.quantity} { grocery_item.price}")
+            totalprice += (int(grocery_item.price))
+            print(f"total price {totalprice}")
 
 
-    def view_all_item(self):
-        print(self.name_store)
-        for item in self.grocery_items:
-            print(item)
+def add_grocery_item():
 
-class Grocery_Item:
-    def __init__(self,itemname,itemquantity,itemprice):
-        self.itemname = itemname
-        self.quantity = itemquantity
-        self.price = itemprice
-    def __repr__(self):
-        return (f'{self.itemname} {self.quantity} {self.price}')
+    display_all_items()
+    try:
+        store_number = int(input("Enter the store numner to add the grocery item:"))
+        selected_store = storeslist[store_number-1]
 
-user_input = ""
-while user_input != "q":
+        item_name = input("Enter the grocery item: ")
+        item_quantity = input("Enter the quantity")
+        item_price = input("Enter the price")
+
+        grocery_item = GroceryItem(item_name,item_quantity,item_price)
+        selected_store.grocery_items.append(grocery_item)
+
+    except IndexError:
+       print("Input the valid store number")
+    # for store in storeslist:
+    #     print(store.name)
+    #     print(store.address)
+
+
+
+
+while user_input != 'q':
     show_menu()
-    user_input = input("enter choice:")
-
+    user_input = input("Enter your choice:")
+    #try:
     if user_input == "1":
         add_store()
     elif user_input == "2":
-         AllStoreList[0].add_item()
+        add_grocery_item()
     elif user_input == "3":
-         AllStoreList[0].view_all_item()
+        display_all_items()
+    #except:
+        #print("Please input only numbers..")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def add_item(self):
+#         for index in range(0,len(AllStoreList)):
+#             print (f"{index + 1}-{AllStoreList[index].name_store}")
+#         additem = input("add your item:")
+#         addquantity = input("add your quantity:")
+#         addprice = input("add price")
+#         grocery_item1 =  Grocery_Item(additem,addquantity,addprice)
+#         self.grocery_items.append(grocery_item1)
+#
+#
+#     def view_all_item(self):
+#         print(self.name_store)
+#         for item in self.grocery_items:
+#             print(item)
+#
+# class Grocery_Item:
+#     def __init__(self,itemname,itemquantity,itemprice):
+#         self.itemname = itemname
+#         self.quantity = itemquantity
+#         self.price = itemprice
+#     def __repr__(self):
+#         return (f'{self.itemname} {self.quantity} {self.price}')
+#
+# user_input = ""
+# while user_input != "q":
+#     show_menu()
+#     user_input = input("enter choice:")
+#
+#     if user_input == "1":
+#         add_store()
+#     elif user_input == "2":
+#         shopping_list1.add_item()
+#     elif user_input == "3":
+#         view_all_item()
